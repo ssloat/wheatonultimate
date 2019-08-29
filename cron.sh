9d33e68544fba8d25fe9b66eace48d7dada97163
Stephen Sloat <stephenlsloat@gmail.com>
2019-08-28 23:32:40 -0500

    add pythonanywhere cron file

---
+++ cron.sh
@@ -0,0 +1,4 @@
+. /home/ssloat/wheatonultimate/setup.sh
+export PYTHONPATH=/home/ssloat/wheaton 
+export PYTHONWARNINGS="ignore:Unverified HTTPS request" 
+/home/ssloat/.virtualenvs/wheaton/bin/python /home/ssloat/wheatonultimate/imap_gmail.py
